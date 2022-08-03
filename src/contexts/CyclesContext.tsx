47a5a4b6142f9678ee8f8c218f1e10da14c8b79d
Diogo Soares <diogo.soares@solarview.com.br>
2022-08-03 18:38:54 -0300

    Ajuste no retorno caso o localStorage esteja undefined

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -59,6 +59,11 @@ export function CyclesContextProvider({
       if (storageStateAsJSON) {
         return JSON.parse(storageStateAsJSON)
       }
+
+      return {
+        cycles: [],
+        activeCycleId: null,
+      }
     },
   )
 
